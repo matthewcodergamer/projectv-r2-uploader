e2357311c0a9f6e30dd9b1536605def84e4d1cf6
matthewcodergamer <gamerstriper@gmail.com>
2026-08-25 17:45:51 -0400

    Bump Project V Uploader cache for cohesive branding

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='project-v-uploader-v7';
-const SHELL=['./','./index.html','./manifest.webmanifest','./favicon-32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+const CACHE='project-v-uploader-v9';
+const SHELL=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET'||u.origin!==self.location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
